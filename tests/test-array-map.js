22a7965422647f9b682cea866d115aee90efcbb8
Sanjeet N. Dasharath <sanjeetdasharath@gmail.com>
2021-07-24 10:43:55 +0530

    pre single run fixes

---
+++ tests/test-array-map.js
@@ -0,0 +1,33 @@
+test("array map", () => {
+    runCode(`
+        let arr = [1, 2, 3];
+        arr = arr.map(function (x) { return x + 5; });
+        assert(arr[0] === 6);
+        assert(arr[1] === 7);
+        assert(arr[2] === 8);
+    `);
+});
+
+test("array map complex callback", () => {
+    runCode(`
+        let arr = [1, 2, 3];
+
+        let x = 5;
+        function bar() {
+            return x;
+        }
+
+        function mapFn(x) {
+            function baz() {
+                return bar() + 1;
+            }
+
+            return x + baz();
+        }
+
+        arr = arr.map(mapFn);
+        assert(arr[0] === 7);
+        assert(arr[1] === 8);
+        assert(arr[2] === 9);
+    `);
+});
